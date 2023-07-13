8661da85283881800a485ef8c6286bccc077b06e
unknown <thiago.dsouza1992@gmail.com>
2023-07-13 16:05:57 -0300

    conclusão do front

--- js/meu-script.js
+++ js/meu-script.js
@@ -35,7 +35,7 @@ window.addEventListener("scroll", function() {
     var navMenu = document.querySelector(".navMenu");
     var navTopo = document.querySelector(".navTopo");
   
-    if (window.scrollY > 0) {
+    if (window.scrollY > 50) {
         navMenu.classList.add("fixed-top");
         navTopo.style.marginBottom = navMenu.offsetHeight + "px";
     } else {
@@ -140,6 +140,39 @@ window.onclick = function(event) {
         openDropdown.style.display = 'none';
       }
     }
+  }
+}
+
+// Meus accordion na página do F.A.Q
+const accordion_item = document.querySelectorAll(".accordion_item");
+
+accordion_item.forEach((item) => {
+  const accordion_header_item = item.querySelector(".accordion_header");
+
+  accordion_header_item.addEventListener("click", () => {
+    const accordion_content_item = item.querySelector(".accordion_content");
+
+    const item_actived = document.querySelector(".active");
+
+    VerifyActive(item, accordion_content_item, item_actived);
+  });
+});
+
+function VerifyActive(item, content, content_actived) {
+  const icon_item = item.querySelector(".icon");
+  const icon_item_active = document.querySelectorAll(".icon");
+
+  icon_item_active.forEach((item) => (item.innerHTML = '<i class="fa-solid fa-plus"></i>'));
+
+  if (content_actived) {
+    content_actived.style.height = 0;
+    content_actived.classList.remove("active");
+  }
+
+  if (content !== content_actived) {
+    icon_item.innerHTML = '<i class="fa-solid fa-minus"></i>';
+    content.classList.add("active");
+    content.style.height = content.scrollHeight + 10 + "px";
   }
 }
 
